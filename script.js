b8f97c309d96714ef82e219c5641d36f99aebb75
ruben <174733720+ruben-23@users.noreply.github.com>
2024-08-10 16:54:34 +0300

    Add event listeners for squares to change color

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const NUMBER_OF_SQUARES = 16;
+let squaresPerSide = 16;
 
 const grid = document.querySelector(".grid");
 
-for (let i=0; i<NUMBER_OF_SQUARES; ++i) {
+for (let i=0; i<squaresPerSide * squaresPerSide; ++i) {
     let square = document.createElement("div");
     square.className = "square";
     grid.appendChild(square);
 }
+const squares = grid.querySelectorAll("div");
+
+squares.forEach( (square) => {
+
+    square.addEventListener( "mouseover", () => {
+        square.style.backgroundColor = 'white';
+    });
+
+} );
